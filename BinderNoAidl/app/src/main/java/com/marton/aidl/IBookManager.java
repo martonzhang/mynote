c7ba420cd394dc3d0f59335b7a9eb8aedf3b5fc4
martonzhang(张雁冰) <martonzhang@tencent.com>
2016-04-19 10:45:25 +0800

    BinderNoAidl

---
+++ BinderNoAidl/app/src/main/java/com/marton/aidl/IBookManager.java
@@ -0,0 +1,135 @@
+package com.marton.aidl;
+
+import android.os.Binder;
+import android.os.IBinder;
+import android.os.IInterface;
+import android.os.Parcel;
+import android.os.RemoteException;
+
+import java.util.List;
+
+/**
+ * Created by marton on 16/4/19.
+ */
+public interface IBookManager extends IInterface {
+
+    static final String DESCRIPTOR = "com.marton.binder.IBookManager";
+
+    static final int TRANSACTION_getBookList = IBinder.FIRST_CALL_TRANSACTION + 0;
+
+    static final int TRANSACTION_addBook = IBinder.FIRST_CALL_TRANSACTION + 1;
+
+    public List<Book> getBookList() throws RemoteException;
+
+    public void addBook(Book book) throws RemoteException;
+
+    public static abstract class Stub extends Binder implements IBookManager{
+
+        public Stub(){
+            this.attachInterface(this,DESCRIPTOR);
+        }
+
+        /**
+         * 将服务端的Ibinder转换为客户端的IbookManager
+         * @param obj
+         * @return
+         */
+        public static IBookManager asInterface(IBinder obj){
+            if (obj == null){
+                return null;
+            }
+            IInterface lin = obj.queryLocalInterface(DESCRIPTOR);
+            if(lin != null && lin instanceof IBookManager){
+                return (IBookManager)lin;
+            }
+
+            return new Proxy(obj);
+        }
+
+        @Override
+        public IBinder asBinder() {
+            return this;
+        }
+
+        @Override
+        protected boolean onTransact(int code, Parcel data, Parcel reply, int flags) throws RemoteException {
+            switch (code){
+                case INTERFACE_TRANSACTION:
+                 reply.writeString(DESCRIPTOR);
+                    return true;
+                case TRANSACTION_getBookList:
+                    data.enforceInterface(DESCRIPTOR);
+                    List<Book> result = this.getBookList();
+                    reply.writeNoException();
+                    reply.writeTypedList(result);
+                    return true;
+                case TRANSACTION_addBook:
+                    data.enforceInterface(DESCRIPTOR);
+                    Book arg0;
+                    if (data.readInt() != 0){
+                        arg0 = Book.CREATOR.createFromParcel(data);
+                    }else{
+                        arg0 = null;
+                    }
+                    this.addBook(arg0);
+                    reply.writeNoException();
+                    return true;
+            }
+            return super.onTransact(code, data, reply, flags);
+        }
+
+        private static class Proxy implements IBookManager{
+            private IBinder mRemote;
+
+            Proxy(IBinder remote){
+                this.mRemote = remote;
+            }
+
+            @Override
+            public List<Book> getBookList() throws RemoteException {
+                Parcel data = Parcel.obtain();
+                Parcel reply = Parcel.obtain();
+                List<Book> result;
+                try{
+                    data.writeInterfaceToken(DESCRIPTOR);
+                    mRemote.transact(TRANSACTION_getBookList,data,reply,0);
+                    reply.readException();
+                    result = reply.createTypedArrayList(Book.CREATOR);
+                }finally {
+                   reply.recycle();
+                    data.recycle();
+                }
+                return result;
+            }
+
+            @Override
+            public void addBook(Book book) throws RemoteException {
+                Parcel data = Parcel.obtain();
+                Parcel reply = Parcel.obtain();
+                try {
+                    data.writeInterfaceToken(DESCRIPTOR);
+                    if (book != null){
+                        data.writeInt(1);
+                        book.writeToParcel(data,0);
+                    }else {
+                        data.writeInt(0);
+                    }
+                    mRemote.transact(TRANSACTION_addBook,data,reply,0);
+                    reply.readException();
+                }finally {
+                    data.recycle();
+                    reply.recycle();
+                }
+            }
+
+            @Override
+            public IBinder asBinder() {
+                return mRemote;
+            }
+
+            public String getInterfaceDescription(){
+                return DESCRIPTOR;
+            }
+        }
+    }
+}
---
+++ BinderNoAidl/app/src/main/java/com/marton/remote/RemoteService.java
@@ -0,0 +1,67 @@
+package com.marton.remote;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.os.RemoteException;
+import android.util.Log;
+
+import com.marton.aidl.Book;
+import com.marton.aidl.IBookManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by marton on 16/4/19.
+ */
+public class RemoteService extends Service {
+
+    private static final String TAG = "RemoteDemo";
+
+    private List<Book> mBookList = new ArrayList<Book>();
+
+    IBookManager.Stub mBinder = new IBookManager.Stub() {
+        @Override
+        public List<Book> getBookList() throws RemoteException {
+            Log.i(TAG,"getBookList in remote service");
+            return mBookList;
+        }
+
+        @Override
+        public void addBook(Book book) throws RemoteException {
+            Log.i(TAG,"addBook in remote service : " + book.toString());
+            mBookList.add(book);
+        }
+    };
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.i(TAG,"onCreate in remote service");
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        Log.i(TAG,"onBind in remote service");
+        return mBinder;
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        Log.i(TAG,"onUnBind in remote service");
+        return super.onUnbind(intent);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.i(TAG,"onStartCommand in remote service");
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.i(TAG,"onDestroy in remote service");
+        super.onDestroy();
+    }
+}
